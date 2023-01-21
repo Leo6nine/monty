1ae3329ba5585f71dede6563896156039bd54b04
Bobson Obiku <leonineobiku@gmail.com>
2023-01-21 15:33:33 -0800

    Tasks

--- monty.h
+++ monty.h
@@ -51,7 +51,7 @@ typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_numberg;
-g instruction_t;
+}instruction_t;
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
